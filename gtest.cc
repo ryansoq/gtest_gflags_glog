b1c85995968c7ea6a1a19fedf38e792eafd38aa6
ymchang <ymchang@andestech.com>
2024-01-12 14:54:38 +0800

    Add gtest

---
+++ gtest.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(ExampleTest, Addition) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(ExampleTest, Subtraction) {
+    EXPECT_EQ(3 - 1, 2);
+}
---
+++ make_gtest.sh
@@ -0,0 +1,1 @@
+g++ gtest.cc -o gtest -std=c++14 -I ./third-party/googletest/usr/include/ -L ./third-party/googletest/usr/lib -lgtest -lgtest_main -pthread
